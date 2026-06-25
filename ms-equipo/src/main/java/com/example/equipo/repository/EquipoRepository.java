04b7e9cbe3a80942d727696d9fb972ee167b518a
toporras <to.porras@duocuc.cl>
2026-06-24 23:40:46 -0400

    Delete ms-equipo

--- ms-equipo/src/main/java/com/example/equipo/repository/EquipoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.equipo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.equipo.model.Equipo;
-
-public interface EquipoRepository extends JpaRepository<Equipo,Integer>{
-
-}
