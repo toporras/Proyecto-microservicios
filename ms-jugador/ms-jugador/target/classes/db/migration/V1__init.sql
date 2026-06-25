862aaa6fee14940c86a86a7b7d13c4ffc87571a3
toporras <to.porras@duocuc.cl>
2026-06-24 23:31:29 -0400

    Delete ms-jugador

--- ms-jugador/ms-jugador/target/classes/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE jugador(
- id INT AUTO_INCREMENT PRIMARY KEY,
- nombre VARCHAR(100),
- posicion VARCHAR(100),
- equipo_id INT
-);
-
-INSERT INTO jugador(nombre, posicion, equipo_id)
-VALUES ('Carlos Diaz', 'Delantero', 1);
--- ms-jugador/src/main/java/com/example/jugador/repository/JugadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jugador.repository;
-
-import com.example.jugador.model.Jugador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface JugadorRepository extends JpaRepository<Jugador, Integer>{
-
-}
