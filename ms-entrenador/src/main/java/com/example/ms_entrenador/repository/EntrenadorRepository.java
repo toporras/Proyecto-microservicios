187f39b5ebad3c297dfa301ff1ca301bbb07467b
toporras <to.porras@duocuc.cl>
2026-06-24 23:39:06 -0400

    Delete ms-entrenador

--- ms-entrenador/src/main/java/com/example/ms_entrenador/repository/EntrenadorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ms_entrenador.repository;
-
-import com.example.ms_entrenador.model.Entrenador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntrenadorRepository extends JpaRepository<Entrenador,Integer> {
-}
--- ms-entrenador/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE entrenador(
- id INT AUTO_INCREMENT PRIMARY KEY,
- nombre VARCHAR(100),
- edad INT,
- equipo_id INT
-);
-
-INSERT INTO entrenador(nombre, edad, equipo_id)
-VALUES ('Jorge Almiron', 52, 1);
