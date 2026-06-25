0c6cc6136c5408a1d1df6c834196aad0d7960687
toporras <to.porras@duocuc.cl>
2026-06-24 23:27:57 -0400

    Implementacion de Swagger y Eureka

---
+++ ms-jugador/ms-jugador/src/main/java/com/example/jugador/repository/JugadorRepository.java
@@ -0,0 +1,9 @@
+package com.example.jugador.repository;
+
+import com.example.jugador.model.Jugador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface JugadorRepository extends JpaRepository<Jugador, Integer>{
+
+}
