6158f87406f099dbcfc1323b65af6a224429300c
toporras <to.porras@duocuc.cl>
2026-06-24 23:33:52 -0400

    Delete ms-estadio

--- ms-estadio/src/main/java/com/example/ms_estadio/repository/EstadioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms_estadio.repository;
-
-
-import com.example.ms_estadio.model.Estadio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadioRepository extends JpaRepository<Estadio,Integer> {
-}
