59dfd964684be8a71fe9083c4076d823a6da7601
toporras <to.porras@duocuc.cl>
2026-06-24 23:29:57 -0400

    Implementacion de Swagger y Eureka

---
+++ ms-equipo/ms-equipo/src/main/java/com/example/equipo/service/EquipoService.java
@@ -0,0 +1,50 @@
+package com.example.equipo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.equipo.model.Equipo;
+import com.example.equipo.repository.EquipoRepository;
+
+@Service
+public class EquipoService {
+
+    @Autowired
+    private EquipoRepository equipoRepository;
+
+    
+    public List<Equipo> listar(){
+        return equipoRepository.findAll();
+    }
+
+    
+    public Equipo buscarPorId(Integer id){
+        return equipoRepository.findById(id).orElse(null);
+    }
+
+    
+    public Equipo guardar(Equipo equipo){
+        return equipoRepository.save(equipo);
+    }
+
+    
+    public Equipo actualizar(Integer id, Equipo equipo){
+
+        Equipo equipo1 = equipoRepository.findById(id).orElse(null);
+
+        if(equipo1 != null){
+            equipo1.setNombre(equipo.getNombre()); 
+            equipo1.setCiudad(equipo.getCiudad());
+            return equipoRepository.save(equipo1);
+        }
+
+        return null;
+    }
+
+   
+    public void eliminar(Integer id){
+        equipoRepository.deleteById(id);
+    }
+}
---
+++ ms-equipo/ms-equipo/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE equipo(
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre VARCHAR(100),
+ ciudad VARCHAR(100)
+);
+
+INSERT INTO equipo(nombre, ciudad)
+VALUES ('Colo Colo','Santiago'),('u de chile','Santiago');
